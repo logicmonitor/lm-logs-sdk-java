caa9e6887e2fdbe89c7326cd18bf2dd96d69c42b
Piotr Melkowski <contractor3deepsense@logicmonitor.com>
2020-09-01 11:32:45 +0200

    Add API builder, response and exception wrappers.

--- src/test/java/com/logicmonitor/logs/LMLogsApiTest.java
+++ src/test/java/com/logicmonitor/logs/LMLogsApiTest.java
@@ -15,7 +15,6 @@
 package com.logicmonitor.logs;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -26,28 +25,39 @@ public class LMLogsApiTest {
         ",      key",
         "id,       ",
     })
-    public void testInvalidConstructorParameters(String accessId, String accessKey) {
-        assertAll(
-            () -> assertThrows(NullPointerException.class, () -> new LMLogsApi(accessId, accessKey)),
-            () -> assertThrows(NullPointerException.class, () -> new LMLogsApi("company", accessId, accessKey))
-        );
+    public void testBuilderInvalidParameters(String accessId, String accessKey) {
+        assertThrows(NullPointerException.class, () -> new LMLogsApi.Builder()
+            .withAccessId(accessId)
+            .withAccessKey(accessKey)
+            .build());
     }
 
-    @Test
-    public void testConstructor() {
+    @ParameterizedTest
+    @CsvSource({
+        ",           0,           1,           true ",
+        "company,    ,            99999999,    false",
+        "company,    1,           ,            true ",
+        "company,    99999999,    0,                ",
+    })
+    public void testBuilder(String company, Integer connectTimeout, Integer readTimeout,
+            Boolean debugging) {
+        LMLogsApi api = new LMLogsApi.Builder()
+            .withAccessId("id")
+            .withAccessKey("key")
+            .withCompany(company)
+            .withConnectTimeout(connectTimeout)
+            .withReadTimeout(readTimeout)
+            .withDebugging(debugging)
+            .build();
+        LMLogsClient client = api.getApiClient();
+
         assertAll(
-            () -> {
-                LMLogsApi api = new LMLogsApi("id", "key");
-                assertNull(api.getApiClient().getCompany());
-            },
-            () -> {
-                LMLogsApi api = new LMLogsApi(null, "id", "key");
-                assertNull(api.getApiClient().getCompany());
-            },
-            () -> {
-                LMLogsApi api = new LMLogsApi("company", "id", "key");
-                assertEquals("company", api.getApiClient().getCompany());
-            }
+            () -> assertEquals(company, client.getCompany()),
+            () -> assertEquals(connectTimeout != null ? connectTimeout : LMLogsClient.DEFAULT_TIMEOUT,
+                    client.getConnectTimeout()),
+            () -> assertEquals(readTimeout != null ? readTimeout : LMLogsClient.DEFAULT_TIMEOUT,
+                    client.getReadTimeout()),
+            () -> assertEquals(debugging != null ? debugging : false, client.isDebugging())
         );
     }
 
